bc9932ba63f6d4d16befbc3cf67328a6a6df80a0
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-02-27 03:18:28 +0600

    Cloudinary media backend fix

--- saleor/core/storages.py
+++ saleor/core/storages.py
@@ -1,3 +1,7 @@
+import os
+
+import cloudinary.uploader
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from django.conf import settings
 from storages.backends.gcloud import GoogleCloudStorage
 from storages.backends.s3boto3 import S3Boto3Storage
@@ -14,3 +18,13 @@ class GCSMediaStorage(GoogleCloudStorage):
     def __init__(self, *args, **kwargs):
         self.bucket_name = settings.GS_MEDIA_BUCKET_NAME
         super().__init__(*args, **kwargs)
+
+
+class CustomMediaCloudinaryStorage(MediaCloudinaryStorage):
+    def _upload(self, name, content):
+        options = {'use_filename': True, 'resource_type': self._get_resource_type(name), 'tags': self.TAG}
+        folder = os.path.dirname(name)
+        if folder:
+            options['folder'] = folder
+        options['unique_filename'] = False
+        return cloudinary.uploader.upload(content, **options)
