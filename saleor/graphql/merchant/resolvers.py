1b223b444fd0758818f62d68fafd9d52ba3a9f11
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-03-13 23:56:49 +0600

    Remove merchant`s featured products field

--- saleor/graphql/merchant/resolvers.py
+++ saleor/graphql/merchant/resolvers.py
@@ -1,13 +1,5 @@
-from saleor.graphql.utils import get_user_or_app_from_context
 from saleor.merchant import models
-from saleor.product import models as product_models
 
 
 def resolve_merchant_by_slug(info, slug):
     return models.Merchant.objects.filter(slug=slug).first()
-
-
-def resolve_featured_products(merchant: models.Merchant, info):
-    requestor = get_user_or_app_from_context(info.context)
-    return product_models.Product.objects.for_merchant(merchant) \
-        .visible_to_user(requestor)
--- saleor/graphql/merchant/types.py
+++ saleor/graphql/merchant/types.py
@@ -1,18 +1,14 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .resolvers import resolve_featured_products
-from ..core.fields import PrefetchingConnectionField
 from ..core.types import Image
 from ..menu.types import Menu
-from ..product.types import Product
 from ...menu.utils import get_merchant_default_nav_menu
 from ...merchant import models
 
 
 class Merchant(DjangoObjectType):
     menu = graphene.Field(type=Menu)
-    featured_products = PrefetchingConnectionField(Product)
     logo = graphene.Field(type=graphene.String, source='get_logo_or_default')
     thumbnail = graphene.Field(
         Image,
@@ -35,10 +31,6 @@ class Merchant(DjangoObjectType):
 
     def resolve_menu(self, info):
         return get_merchant_default_nav_menu(self)
-
-    @staticmethod
-    def resolve_featured_products(root: models.Merchant, info, **_kwargs):
-        return resolve_featured_products(root, info)
 
     @staticmethod
     def resolve_thumbnail(root: models.Merchant, info, size=100, **_kwargs):
